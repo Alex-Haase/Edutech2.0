f5f84974fa054378a4bf31b7767dff907ef998f6
NitroSky07 <137220853+NitroSky07@users.noreply.github.com>
2025-06-08 19:20:08 -0400

    Delete EduTech-main/EduTech-main directory

--- EduTech-main/EduTech-main/EduTech-vm/src/main/java/com/EduTech_vm/cl/EduTech_vm/Repository/MensajeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.EduTech_vm.cl.EduTech_vm.Repository;
-
-import com.EduTech_vm.cl.EduTech_vm.Model.MensajeContacto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MensajeRepository extends JpaRepository<MensajeContacto, Long> {
-    
-}
--- EduTech-main/EduTech-main/README.md
+++
@@ -1,2 +0,0 @@
-# EduTech
-proyecto_web
